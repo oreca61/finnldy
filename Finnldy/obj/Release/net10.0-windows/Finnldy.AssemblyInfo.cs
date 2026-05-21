404f86b89431a8505e8d0006007067a4980a3fd5
Zenbook <Zenbook@ASUS>
2026-05-21 13:50:50 +0200

    Git test

--- Finnldy/obj/Release/net10.0-windows/Finnldy.AssemblyInfo.cs
+++ Finnldy/obj/Release/net10.0-windows/Finnldy.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finnldy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1174c4648559c86098a3536d0b1f59e8823c8ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finnldy")]
