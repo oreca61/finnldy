ab98447ede2469ebec7a70e58b04c6ea881e2062
Zenbook <Zenbook@ASUS>
2026-05-28 15:14:21 +0200

    Kurz was bei Movies geändert dass gg einen neuen Kontruktor bei movies gemachet

--- Finnldy/obj/Debug/net8.0-windows/Finnldy.AssemblyInfo.cs
+++ Finnldy/obj/Debug/net8.0-windows/Finnldy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c082c2290e07bd580b5fa08b786f3310bc9bfd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e3605ddd21a4115c86f0749f20ef8394703a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
