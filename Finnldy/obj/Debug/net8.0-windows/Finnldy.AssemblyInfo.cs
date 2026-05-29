2210747b3a1f7f1db19f2ea2c21aefe0c206a2ed
Zenbook <Zenbook@ASUS>
2026-05-29 14:05:46 +0200

    Movies geupdatet und Lobby controller weiter gemacht

--- Finnldy/obj/Debug/net8.0-windows/Finnldy.AssemblyInfo.cs
+++ Finnldy/obj/Debug/net8.0-windows/Finnldy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e3605ddd21a4115c86f0749f20ef8394703a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab98447ede2469ebec7a70e58b04c6ea881e2062")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
