ffad1b0a58f913230d8efa70c4b1cc8823ca5df3
Zenbook <Zenbook@ASUS>
2026-06-01 11:52:42 +0200

    LobbyControler die Methoden erweitert sollte alles Fertig sein außer Filme füllen methode

--- Finnldy/obj/Debug/net8.0-windows/Finnldy.AssemblyInfo.cs
+++ Finnldy/obj/Debug/net8.0-windows/Finnldy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab98447ede2469ebec7a70e58b04c6ea881e2062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2210747b3a1f7f1db19f2ea2c21aefe0c206a2ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
