29ab4b14f88ffedc63eea865f9911af771ab10ca
Zenbook <Zenbook@ASUS>
2026-06-08 15:00:57 +0200

    database endpuntk aufrufe gemacht + lobby cintoller etwas code aufgeräumt

--- Finnldy/DAL/Database/Database.cs
+++ Finnldy/DAL/Database/Database.cs
@@ -1,35 +1,96 @@
 ﻿using Finnldy.BLL;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net;
 
 namespace Finnldy.DAL.Database
 {
     public class Database
     {
-        public User GetUser(string name)
+        private readonly HttpClient _httpClient;
+
+        public Database()
         {
-            // TODO
-            User user = new User(name);
-            return user;
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:8000");
         }
 
-        public bool IsUserActive(string name)
+        public async Task<List<User>> GetAllUsers()
         {
-            // TODO
-            User user = new User(name);
-            return true;
+            var users = await _httpClient.GetFromJsonAsync<List<User>>("/users/");
+            return users ?? new List<User>();
         }
 
-        public void AddToWatched(User user, Movies movie)
+        public async Task<User?> GetUser(string username)
         {
-            string query = "INSERT INTO Watched (?, ?, ?) VALUES (UserName, Title, WatchedAt);";
+            string encodedUsername = Uri.EscapeDataString(username);
+
+            var response = await _httpClient.GetAsync($"/users/by-name/{encodedUsername}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            return await response.Content.ReadFromJsonAsync<User>();
         }
 
-        private void ConnectToDatabase(string query)
+        public async Task<User?> CreateUser(string name)
         {
+            var request = new 
+            {
+                name = name
+            };
 
+            var response = await _httpClient.PostAsJsonAsync("/users/", request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            return await response.Content.ReadFromJsonAsync<User>();
         }
+
+        public async Task<bool> DeleteUser(int userId)
+        {
+            var response = await _httpClient.DeleteAsync($"/users/{userId}");
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<List<Swipe>> GetUserSwipes(int userId)
+        {
+            var swipes = await _httpClient.GetFromJsonAsync<List<Swipe>>($"/users/{userId}/swipes");
+            return swipes ?? new List<Swipe>();
+        }
+
+        public async Task<List<Swipe>> GetUserWatchedMovies(int userId)
+        {
+            var watched = await _httpClient.GetFromJsonAsync<List<Swipe>>($"/users/{userId}/watched");
+            return watched ?? new List<Swipe>();
+        }
+
+
+        public async Task<bool> IsUserActive(string name)
+            {
+                var response = await _httpClient.GetAsync($"/users/by-name/{name}");
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return true;
+                }
+
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return false;
+                }
+
+                throw new Exception("Fehler beim Prüfen, ob der User existiert.");
+            }
     }
 }
--- Finnldy/obj/Debug/net8.0-windows/Finnldy.AssemblyInfo.cs
+++ Finnldy/obj/Debug/net8.0-windows/Finnldy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654c0bffe292bfc4ea9328fa963ca24be1b4934f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8378d83f5ad37b2cad926f8aef8dfdb3921f6dae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finnldy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
